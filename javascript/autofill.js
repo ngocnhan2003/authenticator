6aec31ce0666d3d4b4f83d40fc740a16f4142416
Nhan N. TRAN <ngocnhan.nplus@gmail.com>
2017-12-16 20:09:27 +0700

    Init

---
+++ javascript/autofill.js
@@ -0,0 +1,25 @@
+'use strict';
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
+$(function() {
+
+  function code2fa() {
+    console.log(getCookie('secrect_2fa'));
+  }  
+
+  var codeInterval = setInterval(code2fa, 1000);
+
+});
---
+++ javascript/real_popup.js
@@ -0,0 +1,1 @@
+(function () { window.open(chrome.extension.getURL("popup.html"), "2FA", "width=320,height=480"); })();
